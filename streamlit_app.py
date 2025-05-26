28304e82d8584948f2b8740b5a1305a319675d5a
Mubashar Ul Hassan <mubasharulhassan00152@gmail.com>
2025-05-26 14:25:28 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import pickle
 
-st.title("Titanic ML App")
-st.write("This is your first Streamlit deployment test.")
+# Model load karo (agar pickle model hai)
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+st.title('My ML Model App')
+
+# Input from user
+input_value = st.number_input('Enter a number')
+
+if st.button('Predict'):
+    prediction = model.predict([[input_value]])
+    st.write(f'Prediction: {prediction[0]}')
+
